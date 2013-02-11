26421ecf79e00a082784dd4fcfb2887717722168
Leroy Bakker <leroybakker@ziggo.nl>
2013-02-11 17:04:42 +0100

    Leroy Bakker
    Added rectangle movement

--- src/com/exonstudio/bakudan/Bakudan.java
+++ src/com/exonstudio/bakudan/Bakudan.java
@@ -1,8 +1,20 @@
 package com.exonstudio.bakudan;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_STENCIL_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.glRectf;
+import static org.lwjgl.opengl.GL11.glTranslatef;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
@@ -16,6 +28,9 @@ public class Bakudan {
 	public static final String TITLE = "Bakudan";
 	public boolean running = false;
 	public long lastTick = 0;
+
+	public int vierkantx;
+	public int vierkanty;
 
 	private DisplayMode size;
 	public TickTimer tickTimer = new TickTimer();
@@ -40,8 +55,33 @@ public class Bakudan {
 		}
 	}
 
+	public void create() throws LWJGLException {
+		// keyboard
+		Keyboard.create();
+	}
+
+	public void moveVierkant() {
+		while (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+			vierkantx += 1;
+			Logger.log("+1 naar rechts");
+		}
+
+		while (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+			vierkanty += 1;
+		}
+
+		while (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			vierkantx -= 1;
+		}
+
+		while (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+			vierkanty -= 1;
+		}
+	}
+
 	public void initGL() {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
+				| GL_STENCIL_BUFFER_BIT);
 
 		glMatrixMode(GL_PROJECTION);
 		glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 1, -1);
@@ -54,7 +94,8 @@ public class Bakudan {
 	}
 
 	public void start() {
-		if (running) return;
+		if (running)
+			return;
 		running = true;
 		gameLoop();
 	}
@@ -79,18 +120,20 @@ public class Bakudan {
 	// Tick update de game, bijvoorbeeld coördinaten van speler en het rondlopen
 	// van mobs.
 	public void tick() {
-		Logger.log("TICK");
+		// Logger.log("TICK");
 	}
 
 	// Render zet alles op het scherm
 	public void render() {
 		Display.update();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glRectf(-0.75f / 30, -0.75f / 30, 0.75f / 30, 0.75f / 30);
+		glRectf(-0.75f + vierkantx, -0.75f + vierkanty, 0.75f + vierkanty,
+				0.75f + vierkanty);
 	}
 
 	public void stop() {
-		if (!running) return;
+		if (!running)
+			return;
 		running = false;
 		System.exit(0);
 	}
