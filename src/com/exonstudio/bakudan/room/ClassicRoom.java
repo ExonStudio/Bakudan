42361fc4e68c394fb5028df9bbaa13a7a23f1a61
Timon de Groot <tdegroot96@gmail.com>
2013-02-11 15:33:55 +0100

    Created Room and ClassicRoom

---
+++ src/com/exonstudio/bakudan/room/ClassicRoom.java
@@ -0,0 +1,14 @@
+package com.exonstudio.bakudan.room;
+
+public class ClassicRoom extends Room {
+
+	/**
+	 * @param width
+	 * @param height
+	 * @param millis
+	 */
+	public ClassicRoom(int width, int height, int millis) {
+		super(width, height, millis);
+	}
+
+}
--- src/com/exonstudio/bakudan/room/Room.java
+++ src/com/exonstudio/bakudan/room/Room.java
@@ -1,18 +1,28 @@
 package com.exonstudio.bakudan.room;
 
+import com.exonstudio.bakudan.room.tile.Tile;
+
 public class Room {
 
 	public int blocks_width = 0;
 	public int blocks_height = 0;
-	public int[] blocks;
+	public Tile[] blocks;
 
 	public long millis = 0;
 
 	public Room(int width, int height, int millis) {
 		blocks_width = width;
 		blocks_height = height;
-		blocks = new int[width * height];
+		blocks = new Tile[width * height];
 		this.millis = millis;
+	}
+
+	public void generateLevel() {
+		for (int y = 0; y < blocks_height; y++) {
+			for (int x = 0; x < blocks_width; x++) {
+
+			}
+		}
 	}
 
 }
