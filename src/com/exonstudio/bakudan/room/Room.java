766758190a20283dd947e2191c39545ff2ac6c5c
Mitchell Ton <mitchellton@gmail.com>
2013-02-17 17:18:26 +0100

    1D array op verzoek van timon.
    cpu drop 12ns/tick

--- src/com/exonstudio/bakudan/room/Room.java
+++ src/com/exonstudio/bakudan/room/Room.java
@@ -7,35 +7,31 @@ public class Room {
 
 	public int blocks_width = 17;
 	public int blocks_height = 15;
-	public Tile[][][] blocks;
+	public Tile[] blocks;
 
 	public long millis = 0;
 
 	public Room(int width, int height, int millis) {
 		blocks_width = width;
 		blocks_height = height;
-		blocks = new Tile[blocks_width + 1][blocks_height + 1][3];
+		blocks = new Tile[blocks_height * blocks_width];
 		this.millis = millis;
 		generateLevel();
 	}
 
 	public void generateLevel() {
-		for (int x = 0; x <= blocks_width; x++) {
-			for (int y = 0; y <= blocks_height; y++) {
-				for (int d = 0; d <= 2; d++) {
-					blocks[x][y][d] = new Tile("stone");
-				}
+		for (int x = 0; x < blocks_width; x++) {
+			for (int y = 0; y < blocks_height; y++) {
+				blocks[x * y] = new Tile("stone");
 			}
 		}
 	}
 
 	public void render() {
-		for (int x = 0; x <= blocks_width; x++) {
-			for (int y = 0; y <= blocks_height; y++) {
-				for (int d = 0; d <= 2; d++) {
-					blocks[x][y][d].selectSprite();
-					SpriteSheet.draw(x * 64, y * 64);
-				}
+		for (int x = 0; x < blocks_width; x++) {
+			for (int y = 0; y < blocks_height; y++) {
+				blocks[x * y].selectSprite();
+				SpriteSheet.draw(x * 64, y * 64);
 			}
 		}
 	}
