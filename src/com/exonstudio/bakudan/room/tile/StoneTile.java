51f60917d8941c52b4f4fdb1ce5a058d2d29dd0e
Timon de Groot <tdegroot96@gmail.com>
2013-02-18 18:55:50 +0100

    Added StoneTile

---
+++ src/com/exonstudio/bakudan/room/tile/StoneTile.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.exonstudio.bakudan.room.tile;
+
+public class StoneTile extends Tile {
+
+	public StoneTile(String sprite) {
+		super(sprite);
+	}
+
+	public boolean isSolid() {
+		return true;
+	}
+
+}
