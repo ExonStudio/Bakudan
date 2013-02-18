86d3456c93054efc9303e49857c04002ffe53e3a
Timon de Groot <tdegroot96@gmail.com>
2013-02-18 19:05:41 +0100

    Added new code to ClassicRoom

--- src/com/exonstudio/bakudan/room/ClassicRoom.java
+++ src/com/exonstudio/bakudan/room/ClassicRoom.java
@@ -1,5 +1,8 @@
 package com.exonstudio.bakudan.room;
 
+import com.exonstudio.bakudan.graphics.SpriteSheet;
+import com.exonstudio.bakudan.room.tile.Tile;
+
 public class ClassicRoom extends Room {
 
 	/**
@@ -9,6 +12,23 @@ public class ClassicRoom extends Room {
 	 */
 	public ClassicRoom(int width, int height, int millis) {
 		super(width, height, millis);
+	}
+
+	public void generateLevel() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				blocks[x + y * width] = Tile.stone;
+			}
+		}
+	}
+
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				blocks[x + y * width].selectSprite();
+				SpriteSheet.draw(x * 64, y * 64);
+			}
+		}
 	}
 
 }
