9e3332e736ec0c7972c7793fdd10c28da68c9085
Timon de Groot <tdegroot96@gmail.com>
2013-02-18 19:05:27 +0100

    Cleaned up Room

--- src/com/exonstudio/bakudan/room/Room.java
+++ src/com/exonstudio/bakudan/room/Room.java
@@ -1,6 +1,5 @@
 package com.exonstudio.bakudan.room;
 
-import com.exonstudio.bakudan.graphics.SpriteSheet;
 import com.exonstudio.bakudan.room.tile.Tile;
 
 public class Room {
@@ -20,20 +19,9 @@ public class Room {
 	}
 
 	public void generateLevel() {
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				blocks[x + y * width] = Tile.stone;
-			}
-		}
 	}
 
 	public void render() {
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				blocks[x + y * width].selectSprite();
-				SpriteSheet.draw(x * 64, y * 64);
-			}
-		}
 	}
 
 }
